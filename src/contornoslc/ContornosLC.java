acd27a3d3c27f52c1f63dd363584683b72ceecd9
fsancheztemprano <fsancheztemprano@nautilus24.danielcastelao.org>
2018-11-22 09:51:12 +0100

    cambios en main rama 3  //cambios en main linea 19-24

--- src/contornoslc/ContornosLC.java
+++ src/contornoslc/ContornosLC.java
@@ -10,17 +10,16 @@ public class ContornosLC {
     
     public static void main(String[] args) {
        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        //cambios en main linea 23
+       
+       
     }
     
+    
+    
+     //cambios en main linea 19
+     //cambios en main linea 20
+     //cambios en main linea 21
+     //cambios en main linea 22
+     //cambios en main linea 23
+     //cambios en main linea 24
 }
